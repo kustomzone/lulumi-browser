8c600491262d82a6bf6c825fda41df818d809f1b
Boik <qazbnm456@gmail.com>
2017-10-13 15:22:32 +0800

    add some handlers for webview tags in Panel.vue

--- src/renderer/components/BrowserMainView/Panel.vue
+++ src/renderer/components/BrowserMainView/Panel.vue
@@ -22,6 +22,44 @@
     windowId: number;
 
     buildPanelPath(extension) {
+      this.$nextTick(() => {
+        const webview = this.$refs[`webview-${extension.extensionId}`][0];
+        webview.addEventListener('context-menu', (event) => {
+          const { Menu, MenuItem } = (this as any).$electron.remote;
+          const menu = new Menu();
+
+          menu.append(new MenuItem({
+            label: 'Inspect Element',
+            click: () => {
+              webview.inspectElement(event.params.x, event.params.y);
+            },
+          }));
+
+          menu.popup((this as any).$electron.remote.getCurrentWindow(), { async: true });
+        });
+        webview.addEventListener('ipc-message', (event: Electron.IpcMessageEvent) => {
+          if (event.channel === 'resize') {
+            const size = event.args[0];
+            webview.style.height = `${size.height}px`;
+            webview.style.width = `${size.width}px`;
+            webview.style.overflow = 'hidden';
+          }
+        });
+        webview.addEventListener('dom-ready', () => {
+          webview.executeJavaScript(`
+            function triggerResize() {
+              const height = document.body.clientHeight;
+              const width = document.body.clientWidth;
+              ipcRenderer.sendToHost('resize', {
+                height,
+                width,
+              });
+            }
+            triggerResize();
+            new ResizeSensor(document.body, triggerResize);
+          `);
+        });
+      });
       return url.format({
         protocol: 'lulumi-extension',
         slashes: true,
